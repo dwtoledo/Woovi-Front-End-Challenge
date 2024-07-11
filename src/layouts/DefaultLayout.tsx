452267a91346f46ef5da2b279da740bc8dc77b06
Douglas Toledo <dwtoledo@outlook.com>
2024-07-11 09:44:38 -0400

    Create a Router Context for Payment Details
    Get Payment Details mocked data

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useOutletContext } from "react-router-dom"
 import { Header } from "../components/Header"
-import { Footer } from "../components/Footer";
+import { Footer } from "../components/Footer"
+import { useState } from "react"
+import { PaymentDetails } from "../models/Payments"
+
+type PaymentDetailsContextType = { 
+  paymentDetails: PaymentDetails | null
+  setPaymentDetails: React.Dispatch<React.SetStateAction<PaymentDetails | null>>
+}
 
 export function DefaultLayout() {
+  const [paymentDetails, setPaymentDetails] = useState<PaymentDetails | null >(null)
+
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet context={{ paymentDetails, setPaymentDetails } satisfies PaymentDetailsContextType} />
       <Footer />
     </>
-  );
+  )
+}
+
+export function usePaymentDetails() {
+  return useOutletContext<PaymentDetailsContextType>();
 }
--- src/pages/PaymentMethod.tsx
+++ src/pages/PaymentMethod.tsx
@@ -1,3 +1,22 @@
+import { useEffect } from "react"
+import { getPaymentDetails } from "../models/Payments"
+import { paymentDetailsRequest } from "../models/MockedData"
+import { usePaymentDetails } from "../layouts/DefaultLayout"
+
 export function PaymentMethod () {
-  return <h2>Payment Method</h2>
+  const {paymentDetails, setPaymentDetails} = usePaymentDetails()
+
+  useEffect(() => {
+    handleGetPaymentDetails()
+  }, [])
+
+  async function handleGetPaymentDetails () {
+    setPaymentDetails(await getPaymentDetails(paymentDetailsRequest))
+  }
+
+  return (
+    <main className="flex flex-col">
+      <p>{JSON.stringify(paymentDetails)}</p>
+    </main>
+  )
 }
