856c62fe1f31cea9dfb61ae176be2fda9c9ca2ac
Douglas Toledo <dwtoledo@outlook.com>
2024-07-10 23:18:33 -0400

    Add Logo & Home Page Link on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,16 @@
-export function Header () {
-  return <h2>Header</h2>
+import { Link } from "react-router-dom"
+import WooviColorfulLogo from "../assets/images/colorful-logo.svg"
+
+export function Header() {
+  return (
+    <header className="flex justify-center items-center my-9">
+      <Link to={"/"}>
+        <img
+          src={WooviColorfulLogo}
+          alt="Logotipo da Woovi"
+          className="h-9 hover:cursor-pointer"
+        />
+      </Link>
+    </header>
+  )
 }
