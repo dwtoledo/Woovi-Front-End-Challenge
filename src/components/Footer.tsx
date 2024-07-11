08d8daf491e18f44d422c1904df148ed4499d5f1
Douglas Toledo <dwtoledo@outlook.com>
2024-07-10 23:51:33 -0400

    Add Footer Content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,12 @@
+
+import ShieldOpaqueIcon from "../assets/icons/opaque-shield-icon.svg"
+import WooviOpaqueLogo from "../assets/images/opaque-logo.svg"
+
 export function Footer () {
-  return <h2>Footer</h2>
+  return (
+  <footer className="flex gap-1 justify-center items-center my-9">
+    <img src={ShieldOpaqueIcon} alt="Ícone de um Escudo" className="h-4" />
+    <span className="font-semibold text-sm text-muted-foreground">Pagamento 100% seguro via:</span>
+    <img src={WooviOpaqueLogo} alt="Woovi Logotipo" className="h-4" />
+  </footer>)
 }
