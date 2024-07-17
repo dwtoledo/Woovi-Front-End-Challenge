13928b1487c26db8ee9f0cff4732812068646df3
Douglas Toledo <dwtoledo@outlook.com>
2024-07-17 16:37:35 -0400

    Implement Payment User Message

--- src/pages/PixCreditCardMethod.tsx
+++ src/pages/PixCreditCardMethod.tsx
@@ -1,3 +1,34 @@
-export function PixCreditCardMethod () {
-  return <h2>Pix Credit Card Method</h2>
+import { useParams } from "react-router-dom"
+import { usePaymentDetails } from "../layouts/DefaultLayout"
+import { brazilBRL } from "../models/Currency"
+
+export function PixCreditCardMethod() {
+  const { paymentId } = useParams()
+  const { paymentDetails } = usePaymentDetails()
+
+  const isUniquePayment = paymentDetails?.payment.id === paymentId
+
+  const installmentDetails = isUniquePayment
+    ? undefined
+    : paymentDetails?.payment.installments.find(
+        (installment) => installment.id === paymentId
+      )
+  
+  function getPaymentMessage() {
+    return (
+      <p className="text-center font-extrabold text-2xl mb-8">
+        {paymentDetails?.user.firstName}, pague{" "}
+        {isUniquePayment && paymentDetails
+          ? `${brazilBRL.format(paymentDetails.payment.total)} `
+          : !!installmentDetails ? `a entrada de ${brazilBRL.format(installmentDetails.value)} ` : ''}
+          pelo Pix.
+      </p>
+    )
+  }
+
+  return (
+    <main className="flex flex-col px-4 items-center">
+      {getPaymentMessage()}
+    </main>
+  )
 }
