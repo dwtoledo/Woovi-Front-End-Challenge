f0f9a740ff9e417c2794dc0f66172a7b64272b60
Douglas Toledo <dwtoledo@outlook.com>
2024-07-19 10:28:12 -0400

    Create Transactions Model

---
+++ src/models/Transactions.ts
@@ -0,0 +1,30 @@
+import { transactionDetailsResponse } from "./MockedData"
+
+export interface TransactionRequest {
+  userId: string,
+  paymentId: string,
+  hasInstallments: boolean,
+  installmentId?: string,
+}
+
+export interface TransactionDetails {
+  userId: string,
+  paymentId: string,
+  transactionId: string,
+  qrCode: string,
+  deadline: string
+}
+
+export async function getTransactionDetails(
+  request: TransactionRequest
+): Promise<TransactionDetails> {
+  try {
+    //const url = import.meta.env.BACKEND_URL + '/transaction/details'
+    //const response: AxiosResponse<TransactionDetails> = await axios.post(url, request)
+    //return response.data
+    return transactionDetailsResponse
+  } catch (error) {
+    alert(`Erro ao solicitar os detalhes da transação: ${error}`)
+    throw error
+  }
+}
